6297c7398809fc81fc16d8c9e14a4c6db5e6133c
Ravindra Nag <ravindranag52@gmail.com>
2022-12-27 09:58:52 +0530

    supabase init

---
+++ src/lib/supabase/client.js
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+import 'dotenv/config'
+
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+
+const supabase = createClient(
+	supabaseUrl,
+	supabaseKey
+)
+
+export default supabase
